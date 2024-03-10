98c4fa311a3ec73ccdd34360de0ee9af9c6b35bd
deivis herrera <herrera.15cortes@gmail.com>
2024-03-09 22:40:05 -0500

    Se creo el archivo UseApi dentro de hooks el cual permite manejar la busqueda, estados y efectos de las busquedas realizadas

--- src/Components/DisplayGifs.jsx
+++ src/Components/DisplayGifs.jsx
@@ -1,7 +1,9 @@
+import { UseApi } from "../hooks/UseApi";
 
 export const DisplayGifs = ({category}) => {
 
   const url = `https://api.giphy.com/v1/gifs/trending?api_key=rL5tWuAJGzNj91tAB0AguE5mmc1Cx6Wv&q=${category}&limit=10`;
+  const { loading, data } = UseApi(url);
 
   return (
     <div className="container-gifs">
